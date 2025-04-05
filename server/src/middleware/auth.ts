4fa9011b833802a5312c61e99399b5d1bbacb50b
gamalmubarak <eng-gemy@hotmail.com>
2025-04-05 17:13:17 -0400

    Complete the authenticateToken method

--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -7,4 +7,18 @@ interface JwtPayload {
 
 export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
   // TODO: verify the token exists and add the user data to the request object
+  const token = req.headers.authorization?.split(' ')[1];
+  if (!token) {
+    return res.status(401).json({ message: 'Access token is missing or invalid' });
+  }
+
+  try {
+    const secretKey = process.env.JWT_SECRET_KEY || '';
+    const decoded = jwt.verify(token, secretKey) as JwtPayload;
+    req.user = { username: decoded.username };
+    next();
+    return;
+  } catch (error) {
+    return res.status(403).json({ message: 'Invalid or expired token' });
+  }
 };
